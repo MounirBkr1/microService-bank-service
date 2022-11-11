d6554a132f782f93be90498970f3b5f0e09c77da
Mounir Bkr <miirbri1@gmail.com>
2022-11-11 16:05:55 +0100

    commit 2 "add spring rest data"

--- src/main/java/com/mnr/ebankservice/repositories/BankAccountRepository.java
+++ src/main/java/com/mnr/ebankservice/repositories/BankAccountRepository.java
@@ -4,6 +4,6 @@ import com.mnr.ebankservice.entities.BankAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-
+@RepositoryRestResource
 public interface BankAccountRepository extends JpaRepository<BankAccount,String> {
 }
--- src/main/java/com/mnr/ebankservice/web/AccountRestController.java
+++ src/main/java/com/mnr/ebankservice/web/AccountRestController.java
@@ -11,7 +11,7 @@ import java.util.UUID;
 
 @RestController
 
-
+@RequestMapping("/api")
 public class AccountRestController {
 
     private BankAccountRepository bankAccountRepository;
